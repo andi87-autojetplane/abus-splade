6e0df2171e50d13fb0da5394faf10c9e2d6e69e9
Wiandi Fazzar <andi87.autojetplane@gmail.com>
2024-09-09 23:04:49 +0700

    Install Laravel Splade

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,13 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('is_banned')->default(0);
+            $table->integer('is_active')->default(0);
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
