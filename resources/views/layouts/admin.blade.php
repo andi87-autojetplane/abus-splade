585789fe3c49a279a1f661fbf032beb3747a839c
Wiandi Fazzar <andi87.autojetplane@gmail.com>
2024-09-20 09:28:22 +0700

    Install Spatie/laravel-query-builder

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,18 +1,5 @@
 <div class="min-h-screen bg-gray-100">
-    @include('layouts.navigation')
-<<<<<<< HEAD
-    <div class="flex space-x-4">
-            <!-- Sidebar -->
-        <Sidebar />
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-
-    </div>
-=======
-
+    @include('layouts.admin-navigation')
     <div class="flex space-x-4">
         <Sidebar />
         <!-- Page Content -->
@@ -20,6 +7,4 @@
             {{ $slot }}
         </main>
     </div>
-
->>>>>>> 4dda2226763d47f32db6b8d0c71c326040c248ee
 </div>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,15 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CityController;
+use App\Http\Controllers\CountryController;
+use App\Http\Controllers\DistrictController;
+use App\Http\Controllers\DusunController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ProvinceController;
+use App\Http\Controllers\SekolahController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VillageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -42,8 +51,17 @@ Route::middleware('splade')->group(function () {
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
 
-    Route::middleware('auth')->group(function () {
-        Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+    Route::middleware('auth')->prefix('/admin')->name('admin.')->group(function () {
+        Route::get('/', [AdminController::class, 'index'])->name('admin.index');
+        Route::resource('/user', UserController::class);
+        Route::resource('/employees', EmployeeController::class);
+        Route::resource('/countries', CountryController::class);
+        Route::resource('/provinces', ProvinceController::class);
+        Route::resource('/cities', CityController::class);
+        Route::resource('/districts', DistrictController::class);
+        Route::resource('/villages', VillageController::class);
+        Route::resource('/dusun', DusunController::class);
+        Route::resource('/sekolah', SekolahController::class);
     });
 
     require __DIR__.'/auth.php';
